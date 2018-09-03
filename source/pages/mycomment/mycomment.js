74c036b5d989629902539a2694255c0bef580583
steve.cai <steve.cai@morningstar.com>
2018-09-03 18:44:49 +0800

    commit

---
+++ source/pages/mycomment/mycomment.js
@@ -0,0 +1,46 @@
+// pages/content/content.js
+import { AppBase } from "../../appbase";
+import { ApiConfig } from "../../apis/apiconfig";
+import { InstApi } from "../../apis/inst.api.js";
+import { PostApi } from "../../apis/post.api.js";
+
+class Content extends AppBase {
+  constructor() {
+    super();
+  }
+  onLoad(options) {
+    this.Base.Page = this;
+    //options.id=5;
+    super.onLoad(options);
+  }
+  onMyShow() {
+    var that = this;
+    this.loadcomment();
+  }
+  loadcomment() {
+    var api = new PostApi();
+    api.commentlist({
+      ismine:"Y",
+      orderby: "r_main.comment_time"
+    }, (commentlist) => {
+      if (this.Base.options.comment_id != undefined) {
+        this.Base.setMyData({
+          comments: commentlist,
+          into_comment_id: "comment_" + this.Base.options.comment_id
+        });
+        this.Base.options.comment_id = undefined;
+      } else {
+
+        this.Base.setMyData({
+          comments: commentlist
+        });
+      }
+    });
+  }
+}
+var content = new Content();
+var body = content.generateBodyJson();
+body.onLoad = content.onLoad;
+body.onMyShow = content.onMyShow;
+body.loadcomment = content.loadcomment;
+Page(body)
